ff6ba91e548144aeaa216b735d05e20815a6012e
HyeonWoo Park <gogoadl@naver.com>
2023-10-31 15:35:46 +0900

    fix : view 디렉토리 구조 변경

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fluid class="text-center ma-0 pa-0">
+    <parallax-banner />
+    <v-container fluid class="ma-6">
+      <v-row>
+        <progress-card title="오늘 온도 봐보자" progressVal="70" />
+        <news-list-card></news-list-card>
+      </v-row>
+    </v-container>
+
+    <kakao-map-wrapper></kakao-map-wrapper>
+  </v-container>
+</template>
+<script>
+import KakaoMapWrapper from '../components/KakaoMapWrapper.vue'
+import NewsListCard from '../components/NewsListCard.vue'
+import ParallaxBanner from '../components/ParallaxBanner.vue'
+import ProgressCard from '../components/ProgressCard.vue'
+
+export default {
+  name: 'ComponentHome',
+  components: {
+    NewsListCard,
+    ProgressCard,
+    KakaoMapWrapper,
+    ParallaxBanner,
+  },
+}
+</script>
+
+<style>
+.v-container {
+  padding: 10px;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container class="fill-height">
+    <v-row justify="center" align="center">
+      <v-card class="mx-auto" max-width="400">
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-subtitle class="text-h5">
+              간편하게 로그인하고
+            </v-list-item-subtitle>
+            <v-list-item-title>다양한 서비스를 이용하세요</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-form class="mt-12">
+          <v-text-field label="Email" name="Email" outlined type="text" />
+          <v-spacer></v-spacer>
+          <v-text-field
+            id="password"
+            label="Password"
+            name="password"
+            outlined
+            type="password"
+          />
+        </v-form>
+
+        <v-divider></v-divider>
+
+        <v-btn text>아이디 찾기</v-btn>
+        <v-btn text>비밀번호 찾기</v-btn>
+        <v-btn text>회원가입 하기</v-btn>
+
+        <v-btn class="mt-12" block outlined large>시작하기</v-btn>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'LoginVue',
+  data: () => ({
+    step: 1,
+  }),
+  props: {
+    source: String,
+  },
+}
+</script>
