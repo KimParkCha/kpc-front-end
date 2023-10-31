8c7c3d90f0184def258348a122a47ade1b4eba75
HyeonWoo Park <gogoadl@naver.com>
2023-10-31 09:08:52 +0900

    add : kakaoMap Component 추가

---
+++ src/components/KakaoMapWrapper.vue
@@ -0,0 +1,15 @@
+<template>
+  <kakao-map></kakao-map>
+</template>
+<script>
+import KakaoMap from './KakaoMap.vue'
+
+export default {
+  name: 'KakaoMapWrapper',
+  components: {
+    KakaoMap,
+  },
+}
+</script>
+
+<style></style>
---
+++ src/components/MapHeader.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+export default {
+  name: 'MapHeader',
+}
+</script>
+
+<style></style>
