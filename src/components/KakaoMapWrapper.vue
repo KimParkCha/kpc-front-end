7e21353a827802a61b89a61c8222bba0393ad43f
HyeonWoo Park <gogoadl@naver.com>
2023-10-31 15:36:35 +0900

    feat : 맵뷰 헤더 추가 및 뉴스리스트 컴포넌트 추가

--- src/components/KakaoMapWrapper.vue
+++ src/components/KakaoMapWrapper.vue
@@ -1,13 +1,18 @@
 <template>
-  <kakao-map></kakao-map>
+  <v-container fluid class="text-center ma-0 pa-0">
+    <map-header />
+    <kakao-map />
+  </v-container>
 </template>
 <script>
 import KakaoMap from './KakaoMap.vue'
+import MapHeader from './MapHeader.vue'
 
 export default {
   name: 'KakaoMapWrapper',
   components: {
     KakaoMap,
+    MapHeader,
   },
 }
 </script>
---
+++ src/components/NewsListCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card class="mx-auto" max-width="400">
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h6"
+          >오늘의 부동산 온도 정보를 확인하세요!</v-list-item-title
+        >
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list class="transparent">
+      <v-list-item v-for="item in forecast" :key="item.day">
+        <v-list-item-title>{{ item.day }}</v-list-item-title>
+
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-subtitle class="text-right">
+          {{ item.temp }}
+        </v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-btn text> Full Report </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'NewsListCard',
+  data() {
+    return {
+      labels: ['SU', 'MO', 'TU', 'WED', 'TH', 'FR', 'SA'],
+      time: 0,
+      forecast: [
+        {
+          day: 'Tuesday',
+          icon: 'mdi-white-balance-sunny',
+          temp: '24\xB0/12\xB0',
+        },
+        {
+          day: 'Wednesday',
+          icon: 'mdi-white-balance-sunny',
+          temp: '22\xB0/14\xB0',
+        },
+        { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
