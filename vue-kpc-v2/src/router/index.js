255356a07731b599c8e2ff0265377f7e14c824bd
HyeonWoo Park <gogoadl@naver.com>
2023-11-15 17:36:19 +0900

    feat : MapView 분리, 라우터 이동 이벤트 불가능 현상 수정

--- vue-kpc-v2/src/router/index.js
+++ vue-kpc-v2/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import MapView from '../views/MapView.vue'
+import MyPageView from '../views/MyPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,27 +13,24 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue')
+      component: LoginView
     },
     {
       path: '/create',
       name: 'create',
-      component: () => import('../views/CreateView.vue')
+      component: MapView
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: MapView
     },
     {
       path: '/mypage',
       name: 'mypage',
-      component: () => import('../views/MyPageView.vue')
+      component: MyPageView
     }
   ]
 })
