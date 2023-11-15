cedd7509eaf66cf79d79b472a6c6de35b29e871d
sycha11 <sycha112@naver.com>
2023-11-16 02:27:02 +0900

    fix : store/user.js loginUser 수정

--- vue-kpc-v2/src/stores/user.js
+++ vue-kpc-v2/src/stores/user.js
@@ -1,10 +1,13 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import userAPI from '@/api/user'
+import { jwtDecode } from 'jwt-decode'
 
 export const useUserStore = defineStore('user', () => {
   const orgUser = ref(null)
   const orgToken = ref(null)
+  const isLogin = ref(false)
+  const isVaildToken = ref(false)
 
   const user = computed(() => {
     return orgUser.value
@@ -19,24 +22,36 @@ export const useUserStore = defineStore('user', () => {
   const setToken = (token) => {
     orgToken.value = token
   }
-  const login = (user) => {
-    setUser(user)
-    setToken('토큰입니다')
-    return user
-    // userAPI.loginUser(
-    //   user,
-    //   ({ data }) => {
-    //     console.log(data)
-    //     console.log('로그인 성공')
-    //     setUser(data.user)
-    //     setUser(data.token)
-    //     router.push({ name: 'home' })
-    //   },
-    //   () => {
-    //     console.log('로그인 실패')
-    //   }
-    // )
+  const login = async (userInfo) => {
+    console.log('store login')
+    await userAPI.loginUser(
+      userInfo,
+      ({ data }) => {
+        console.log(data)
+        console.log('data response' + data.response)
+        console.log('data message ' + data.message)
+        console.log('data ref ' + data.data)
+
+        if (data.response === 'success') {
+          // setUser(data.user)
+          setToken(data.token)
+          console.log('success5')
+          sessionStorage.setItem('accessToken', data.accessToken)
+          sessionStorage.setItem('refreshToken', data.refreshToken)
+          console.log("sessiontStorage에 담았다", isLogin.value);
+        } else {
+          console.log('로그인 실패2')
+          isLogin.value = false
+          isVaildToken.value = false
+        }
+      },
+      (error) => {
+        console.error(error)
+      }
+    )
   }
 
-  return { user, token, login }
+
+
+  return { user, token, isLogin, isVaildToken, login }
 })
--- vue-kpc-v2/src/views/LoginView.vue
+++ vue-kpc-v2/src/views/LoginView.vue
@@ -1,44 +1,46 @@
 <script setup>
-import { reactive } from 'vue'
+import { ref, reactive } from 'vue'
 import userAPI from '@/api/user'
 import { useRouter} from 'vue-router'
 import { useUserStore } from '@/stores/user'
 import { storeToRefs } from 'pinia'
-const store = useUserStore()
+const userStore = useUserStore()
 
-const { user: globalUser, token: globalToken } = storeToRefs(store)
-const {login: globalLogin} = store
+const { user: globalUser, token: globalToken } = storeToRefs(userStore)
+const { isLogin } = storeToRefs(userStore)
+
+const { login } = userStore
+
+const loginUser = ref({
+  email: '',
+  password: ''
+})
 
 console.log(globalUser.value)
 console.log(globalToken.value)
-globalLogin(globalUser.value)
-console.log(globalUser.value)
-console.log(globalToken.value)
-
+// globalLogin(globalUser.value)
+// console.log(globalUser.value)
+// console.log(globalToken.value)
 
 const router = useRouter()
 const user = reactive({})
-const login = () => {
-  userAPI.loginUser(
-    user,
-    ({ data }) => {
-      console.log(data.response)
-      console.log(data.message)
-      console.log(data.data)
-      if (data.response == 'success') {
-        localStorage.setItem('token', data.data)
-        localStorage.setItem('user', JSON.stringify(data.user))
-        router.push({ name: 'home' })
-      }
-      if (data.response == 'error') {
-        alert(data.message)
-        router.push({ name: 'login' })
-      }
-    },
-    () => {
-      console.log('로그인 실패')
-    }
-  )
+const loginFn = async () => {
+  console.log(loginUser.value.email)
+  console.log(loginUser.value.password)
+  console.log('login ing!!!! !!!')
+
+  await login(loginUser.value)
+  let token = sessionStorage.getItem('token')
+  
+  console.log('111. ', token)
+  console.log('isLogin: ', isLogin)
+  if (isLogin) {
+    console.log('로그인 성공아닌가???')
+    router.push('/')
+  } else {
+    console.log('로그인 실패')
+    router.push('/login')
+  }
 }
 </script>
 
@@ -47,7 +49,7 @@ const login = () => {
     <v-card class="mx-auto px-6 py-8">
       <v-form v-model="form" @submit.prevent="onSubmit">
         <v-text-field
-          v-model.lazy="user.email"
+          v-model.lazy="loginUser.email"
           :readonly="loading"
           :rules="[required]"
           class="mb-2"
@@ -56,7 +58,7 @@ const login = () => {
         ></v-text-field>
 
         <v-text-field
-          v-model.lazy="user.password"
+          v-model.lazy="loginUser.password"
           :readonly="loading"
           :rules="[required]"
           clearable
@@ -74,7 +76,7 @@ const login = () => {
           size="large"
           type="submit"
           variant="elevated"
-          @click="login"
+          @click="loginFn"
         >
           Sign In
         </v-btn>
