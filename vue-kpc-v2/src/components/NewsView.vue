957d8430a51f24e388d1f71ff13afe615fd47690
sycha11 <sycha112@naver.com>
2023-11-20 11:37:12 +0900

    fix: map 단지정보 UI 수정

--- vue-kpc-v2/src/components/NewsView.vue
+++ vue-kpc-v2/src/components/NewsView.vue
@@ -1,12 +1,12 @@
 <script setup>
-const variants = ['elevated', 'flat', 'tonal', 'outlined']
+const variants = ['뉴스1', '뉴스2', '뉴스3', '뉴스4']
 </script>
 
 <template>
   <v-container>
     <v-row align="center" justify="center">
       <v-col v-for="(variant, i) in variants" :key="i" cols="auto">
-        <v-card class="mx-auto" max-width="344" :variant="variant">
+        <!-- <v-card class="mx-auto" max-width="344" :variant="variant">
           <v-card-item>
             <div>
               <div class="text-overline mb-1">
@@ -24,7 +24,19 @@ const variants = ['elevated', 'flat', 'tonal', 'outlined']
           <v-card-actions>
             <v-btn> Button </v-btn>
           </v-card-actions>
-        </v-card>
+        </v-card> -->
+        <v-card
+          class="mx-auto my-8"
+          max-width="344"
+          title="Vuetify on GitHub"
+          append-icon="mdi-open-in-new"
+          href="https://n.news.naver.com/article/417/0000963239?cds=news_media_pc&type=editn"
+          target="_blank"
+          rel="noopener"
+          :variant="variant"
+        >
+          {{ variant }}</v-card
+        >
       </v-col>
     </v-row>
   </v-container>
