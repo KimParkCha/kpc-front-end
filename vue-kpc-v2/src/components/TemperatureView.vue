37193634ac46eaf7801e6ec7b03fd56a2ecbdc39
HyeonWoo Park <gogoadl@naver.com>
2023-11-21 18:18:05 +0900

    feat : 온도계 컴포넌트 추가

---
+++ vue-kpc-v2/src/components/TemperatureView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import '@/assets/test.css'
+import { ref } from 'vue'
+const value = ref(0)
+let timerId = null
+const progressAnimate = () => {
+  const btn = document.getElementById('btn')
+  setInterval(() => {
+    const progress = document.getElementById('th-animated')
+    value.value += 1
+  }, 100)
+  btn.disabled = true
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="thermometer">
+      <div class="thermometer-body">
+        <progress max="100" value="25"></progress>
+      </div>
+    </div>
+    <div class="thermometer">
+      <div class="thermometer-body">
+        <progress max="100" value="75"></progress>
+      </div>
+    </div>
+    <div class="thermometer">
+      <div class="thermometer-body">
+        <progress max="100" value="100"></progress>
+      </div>
+    </div>
+    <div class="animated primary">
+      <button name="button" id="btn" @click="progressAnimate(30)">Start progress</button>
+      <div class="thermometer">
+        <div class="thermometer-body">
+          <progress max="100" :value="value" id="th-animated"></progress>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped></style>
